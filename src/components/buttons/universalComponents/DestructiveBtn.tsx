102dc85cdfe6726fc3746d70c384d45aa670d77f
teiatan <protas.tb@gmail.com>
2023-10-13 14:21:12 +0300

    feat: пропс стилів для кнопки

--- src/components/buttons/universalComponents/DestructiveBtn.tsx
+++ src/components/buttons/universalComponents/DestructiveBtn.tsx
@@ -6,13 +6,15 @@ const DestructiveBtn = ({
     to,
     disabled,
     // pressed,
-    onClick
+    onClick,
+    buttonStyle
 }:{
     children?:ReactElement | string;
     to?:string;
     disabled?:boolean;
     pressed?:boolean;
     onClick?:()=>void;
+    buttonStyle?:string;
     }) => {
 
     const navigate = useNavigate();
@@ -28,13 +30,14 @@ const DestructiveBtn = ({
 
     return (
         <button
-            className="font-sans font-semibold px-l py-xs text-base leading-6 tracking-button
+            className={`font-sans font-semibold px-l py-xs text-base leading-6 tracking-button
                 text-textColors-contrast  bg-bgColors-defaultDestructive
                 hover:bg-bgColors-hoverDestructive focus:outline-none 
                 focus:drop-shadow-focusButton rounded-medium transition-colors duration-300
 
                 disabled:text-textColors-disabled disabled:bg-bgColors-disable disabled:border disabled:border-solid
-                disabled:border-borderColors-disabled disabled:rounded-medium disabled:cursor-not-allowed"
+                disabled:border-borderColors-disabled disabled:rounded-medium disabled:cursor-not-allowed
+                ${buttonStyle}`}
             onClick={handleButtonClick}
             disabled={disabled}
         >
