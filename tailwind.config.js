53238d5189a8bad808420850c5a316c143b6757a
teiatan <protas.tb@gmail.com>
2023-10-14 10:20:10 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 import theme from "./presets";
 export default {
   content: [
-    './src/**/*.{html,js,jsx,ts,tsx}',
+    './src/**/*.{html,js,jsx,ts,tsx,mdx}',
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme,
   plugins: [],
