6fbb8b6f60c5a635cdb1496a53e96394e99fa543
koseckaya <kosetsaya@gmail.com>
2023-10-27 16:27:43 +0300

    fix: fix color prop in LoaderIcon

--- src/components/icons/universalComponents/LoaderIcon.tsx
+++ src/components/icons/universalComponents/LoaderIcon.tsx
@@ -8,10 +8,36 @@ const LoaderIcon = ({
     size?: string;
     }) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={size} height={+size/4} viewBox="0 0 64 16" fill="none">
-            <circle opacity="0.2" cx="8" cy="8" r="8" fill={"currentColor"} className="animate-pulse"/>
-            <circle opacity="0.5" cx="32" cy="8" r="8" fill={color} className="animate-pulse" />
-            <circle cx="56" cy="8" r="8" fill={color} className="animate-pulse"/>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={size}
+            height={+size / 4}
+            viewBox="0 0 64 16"
+            fill="none"
+        >
+            <circle
+                opacity="0.2"
+                cx="8"
+                cy="8"
+                r="8"
+                fill={color}
+                className="animate-pulse"
+            />
+            <circle
+                opacity="0.5"
+                cx="32"
+                cy="8"
+                r="8"
+                fill={color}
+                className="animate-pulse"
+            />
+            <circle
+                cx="56"
+                cy="8"
+                r="8"
+                fill={color}
+                className="animate-pulse"
+            />
         </svg>
     );
 };
