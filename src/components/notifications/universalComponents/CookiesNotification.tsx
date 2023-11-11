d1f9e2296b666c76264df1b0ef44ededa2cbffe7
Khrystyna <khrystyna.makar@gmail.com>
2023-11-11 19:30:15 +0200

    added link

--- src/components/notifications/universalComponents/CookiesNotification.tsx
+++ src/components/notifications/universalComponents/CookiesNotification.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react"
+import { FC } from "react"
 import { Sizes } from './Notification'
 
 interface CookiesNotificationProps {
@@ -10,13 +10,15 @@ const CookiesNotification: FC<CookiesNotificationProps> =
     ({ size = Sizes.Big, className = "" }: CookiesNotificationProps) => {
 
         let sizeStyle = "w-[495px]";
-        if(size == Sizes.Small){
+        if (size == Sizes.Small) {
             sizeStyle = "w-[300px]"
         }
 
         return (
             <div className={`fixed bottom-[24px] left-[50%] -translate-x-[50%] w-fit border-1 border-bgBrandDark rounded-medium3 px-[16px] py-[12px] ${className}`}>
-                <p className={`text-textPrimary ${sizeStyle}`}>Ми використовуємо cookies, аби сайт був кращим. <span className="text-textBrand">Що це?</span></p>
+                <p className={`text-textPrimary ${sizeStyle}`}>Ми використовуємо cookies, аби сайт був кращим.{' '}
+                    <a href='https://uk.wikipedia.org/wiki/%D0%9A%D1%83%D0%BA%D0%B8' target="_blank" className="hover:text-textBrand hover:underline" ><span className="text-textBrand">Що це?</span></a>
+                </p>
             </div>
         )
     }
