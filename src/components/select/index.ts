b1e01fc8938efd5ebafca320ffda137e31bb6ed7
teitan <protas.tb@gmail.com>
2024-03-19 14:20:13 +0200

    fix: reexport Multiselect

--- src/components/select/index.ts
+++ src/components/select/index.ts
@@ -1,7 +1,9 @@
 import { Dropdown } from "./Dropdown";
 import { Combobox } from "./Combobox";
+import Multiselect from "./Multiselect/Multiselect";
 
 export { 
     Dropdown,
-    Combobox 
+    Combobox,
+    Multiselect
 }
